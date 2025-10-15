56f16642b065d338e505f49a2b7d973468de671e
Anmeng12 <1283291837@qq.com>
2025-10-15 14:18:35 +0800

    Add files via upload

---
+++ shangji 2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+	// 2.(1)
+	printf("*********************\n");
+	printf("very good!\n");
+	printf("*********************\n");
+
+
+	printf("****************\n very good!\n****************\n");
+
+
+
+
+
+
+	//2.(2)
+	int a, b, sum, minus;
+	a = 123; b = 456;
+	sum = a + b; minus = a - b;
+	printf("a+b=%d\na-b=%d\n", sum, minus);
+	
+
+	// printf中删除a+b=和a-b=  ：
+	
+	
+	// printf 中删除第一个\n 
+
+
+	return 0;
+
+
+
+}
